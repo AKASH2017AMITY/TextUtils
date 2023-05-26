8e190f9d207c83c23298a5f5d2b0ffeb3b75fd35
akash kumar <120650503+AKASH2017AMITY@users.noreply.github.com>
2023-05-26 14:29:44 +0530

    Updates

---
+++ precache-manifest.b0ef6a9a1b8bdebdf4c03d4b4217d07d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "52e00d213ea4f9477280",
+    "url": "/myfirstapp/static/css/main.740c274d.chunk.css"
+  },
+  {
+    "revision": "52e00d213ea4f9477280",
+    "url": "/myfirstapp/static/js/main.4e2c1c25.chunk.js"
+  },
+  {
+    "revision": "6b7b7b1168dddc3c52db",
+    "url": "/myfirstapp/static/js/runtime~main.c37d5795.js"
+  },
+  {
+    "revision": "a4fe427394bf3e94e74d",
+    "url": "/myfirstapp/static/js/2.efe9a09f.chunk.js"
+  },
+  {
+    "revision": "9e493117a3dfb5238748",
+    "url": "/myfirstapp/static/js/3.f326d3da.chunk.js"
+  },
+  {
+    "revision": "87cb7f02edf27d41a2ba47dc9e84ae6c",
+    "url": "/myfirstapp/index.html"
+  }
+];
